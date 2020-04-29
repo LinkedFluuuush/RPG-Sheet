96b225fe52b6eeb7eaf3181a31cbf8ff2ce631f0
Pierre Leboucher <leboucher.p@gmail.com>
2020-04-29 21:41:14 +0200

    Exit Mac app on window close

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ if (handleSquirrelEvent()) {
 
 const { app, BrowserWindow } = require("electron");
 let win;
-const isMac = process.platform === "darwin";
 
 const { createMenu } = require("./services/menu");
 
@@ -30,26 +29,14 @@ function createWindow() {
   win.loadFile("./src/view/index.html");
 }
 
-// Cette méthode sera appelée quant Electron aura fini
+// Cette méthode sera appelée quand Electron aura fini
 // de s'initialiser et prêt à créer des fenêtres de navigation.
-// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+// Certaines APIs peuvent être utilisées uniquement quand cet événement est émis.
 app.whenReady().then(createWindow);
 
 // Quitter si toutes les fenêtres ont été fermées.
 app.on("window-all-closed", () => {
-  // Sur macOS, il est commun pour une application et leur barre de menu
-  // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
-  if (!isMac) {
     app.quit();
-  }
-});
-
-app.on("activate", () => {
-  // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
-  // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
-  if (win === null) {
-    createWindow();
-  }
 });
 
 // Dans ce fichier, vous pouvez inclure le reste de votre code spécifique au processus principal. Vous pouvez également le mettre dans des fichiers séparés et les inclure ici.
